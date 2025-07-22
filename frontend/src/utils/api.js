2920aef2beb335673fac5c3617b9c1f2d7d35020
hirthic03 <hirthic1517@gmail.com>
2025-07-22 17:19:04 +0800

    Initial commit - server.js with secure JWT auth

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'https://jpa-data-confirmation-system-v1.onrender.com',
 });
 
 export default api;
